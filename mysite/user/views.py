3058c8d7843db3c45a37f8846195ad6ae353e245
Ahmmod097 <rifatsadique@gmail.com>
2021-06-28 12:07:21 +0630

    First Push

---
+++ mysite/user/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
+from django.contrib.auth.forms import UserCreationForm
+
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
+from django.contrib.auth.decorators import login_required
+
+from .models import *
+from .forms import CreateUserForm
+
+
+def registerPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		form = CreateUserForm()
+		if request.method == 'POST':
+			form = CreateUserForm(request.POST)
+			if form.is_valid():
+				form.save()
+				user = form.cleaned_data.get('username')
+				messages.success(request, 'Account was created for ' + user)
+
+				return redirect('login')
+			
+
+		context = {'form':form}
+		return render(request, 'user/register.html', context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				return redirect('nothome')
+
+		context = {}
+		return render(request, 'user/login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
+
+
+def home(request):
+	context1 = request.user
+	return render(request, 'user/home.html', {'context1':context1})
+
+def nothome(request):
+	
+	return render(request, 'user/nothome.html')
+
